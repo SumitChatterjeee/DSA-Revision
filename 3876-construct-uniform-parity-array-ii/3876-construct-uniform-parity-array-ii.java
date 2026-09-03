8dd626e50c1e47a1b1b440c3c0c76a1b7dcd3116
Sumit Chatterjee <subrothochatt123@gmail.com>
2026-09-04 00:13:44 +0530

    Time: 66 ms (5.07%), Space: 125.6 MB (5.07%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,19 +1,81 @@
 class Solution {
+    public boolean checkAllEven(int arr[]){
+        int n=arr.length;
 
-    public boolean uniformArray(int[] nums1) {
-        int mn = nums1[0];
-        boolean hasOdd = false;
-        for (int v : nums1) {
-            if (v < mn) {
-                mn = v;
-            }
-            if ((v & 1) == 1) {
-                hasOdd = true;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==1){
+                return false;
             }
         }
-        if ((mn & 1) == 1) {
+        return true;
+    }
+    public boolean checkAllOdds(int arr[]){
+        int n=arr.length;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean getCount(ArrayList<Integer>arr,int k){
+        int n=arr.size();
+        int left=0;
+        int right=n-1;
+        int count=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(arr.get(mid)==k){
+                right=mid-1;
+            }
+            else if(arr.get(mid)>k){
+                right=mid-1;
+            }
+            else{
+                count+=mid-left+1;
+                left=mid+1;
+            }
+
+            if(count>=1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public boolean uniformArray(int[] nums1) {
+        ArrayList<Integer>arr=new ArrayList<>();
+
+        for(int i:nums1){
+            if(i%2==1){
+                arr.add(i);
+            }
+        }
+
+        Collections.sort(arr);
+
+        if(checkAllEven(nums1)){
             return true;
         }
-        return !hasOdd;
+
+         if(checkAllOdds(nums1)){
+            return true;
+        }
+
+
+
+        int n=nums1.length;
+
+        for(int i=0;i<n;i++){
+            if(nums1[i]%2==0){
+                if(!getCount(arr,nums1[i])){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
     }
 }
